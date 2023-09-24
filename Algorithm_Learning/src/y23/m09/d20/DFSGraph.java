8735e4798bfa37c6b8d9de12c909ec9a7062a56b
Echoget <czy13572125800@163.com>
2023-09-25 00:30:40 +0800

    新增图的DFS

--- Algorithm_Learning/src/y23/m09/d20/DFSGraph.java
+++ Algorithm_Learning/src/y23/m09/d20/DFSGraph.java
@@ -1,4 +1,32 @@
 package y23.m09.d20;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+
 public class DFSGraph {
+
+    public static void dfs(Node node){
+        if(node == null){
+            return;
+        }
+        Deque<Node> s = new ArrayDeque<>();
+        HashSet<Node> set = new HashSet<>();
+        s.push(node);
+        set.add(node);
+        System.out.println(node.id);
+        while (!s.isEmpty()){
+            Node cur = s.pop();
+            for (Node next : cur.nexts){
+                if(!set.contains(next)){
+                    s.push(cur);
+                    s.push(next);
+                    set.add(next);
+                    System.out.println(next.id);
+                    break;
+                }
+            }
+        }
+
+    }
 }
